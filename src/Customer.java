87ea4781bc072c2a05defe9aa3cbebe8d04e561a
mark_us <markus.hauf@live.de>
2016-04-29 17:47:09 +0200

    Extract Method getFrequentRenterPoints

--- src/Customer.java
+++ src/Customer.java
@@ -25,11 +25,8 @@ class Customer {
         while (enum_rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
-            // add frequent renter points
-            frequentRenterPoints ++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
-                frequentRenterPoints ++;
+            frequentRenterPoints = getFrequentRenterPoints(frequentRenterPoints, each);
+
             //show figures for this rental
             //replace parameter with explicit methods
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
@@ -39,6 +36,16 @@ class Customer {
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
+    }
+
+    //Extract Method getFrequentRenterPoints
+    private int getFrequentRenterPoints(int frequentRenterPoints, Rental each) {
+        // add frequent renter points
+        frequentRenterPoints ++;
+        // add bonus for a two day new release rental
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+            frequentRenterPoints ++;
+        return frequentRenterPoints;
     }
 
     /*
