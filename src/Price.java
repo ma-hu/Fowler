a5e8b51b320affc503bafd584575ff044ae9caf5
mark_us <markus.hauf@live.de>
2016-04-29 18:46:00 +0200

    make method abstact

--- src/Price.java
+++ src/Price.java
@@ -4,24 +4,6 @@
 abstract class Price {
     abstract int getPriceCode();
 
-    //move method -> getCharge from move to price
-    double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
-    }
+    //make method abstact
+   abstract double getCharge(int daysRented);
 }
